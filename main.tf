aae47e067b0b6d2d1ef5ccbc432f0495087bc333
doremansunio <136595579+doremansunio@users.noreply.github.com>
2024-10-01 15:08:16 +0530

    Update main.tf5

--- main.tf
+++ main.tf
@@ -2,12 +2,3 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
-resource "aws_s3_bucket" "example" {
-  bucket = "pk-bucket-01"
-
-  tags = {
-    Name        = "phanibucket"
-    Environment = "Dev"
-  }
-}
